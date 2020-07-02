c715735fd02c0290f908b008c018361702aeccc6
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-07-02 16:36:12 +0200

    replacing dagger with hilt

--- core/src/main/java/com/skybox/seven/core/di/MalModule.kt
+++ core/src/main/java/com/skybox/seven/core/di/MalModule.kt
@@ -2,15 +2,11 @@ package com.skybox.seven.core.di
 
 import com.google.gson.GsonBuilder
 import com.skybox.seven.core.mal.api.MALService
-import dagger.Module
-import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-@Module
 class MalModule {
-    @Provides
     fun providesMalService(client: Lazy<OkHttpClient>): MALService =
         Retrofit.Builder()
             .baseUrl(MALService.ENDPOINT)
