ba16421ae28c2fe051ab888eb1eb82f610623630
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-07-02 10:26:37 +0200

    client interceptor

--- core/src/main/java/com/skybox/seven/core/mal/api/ClientInterceptor.kt
+++ core/src/main/java/com/skybox/seven/core/mal/api/ClientInterceptor.kt
@@ -1,5 +1,22 @@
 package com.skybox.seven.core.mal.api
 
-class ClientInterceptor {
+import com.skybox.seven.core.mal.login.AuthTokenDataSource
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class ClientInterceptor(private val tokenDataSource: AuthTokenDataSource, private val client_id: String): Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val requestBuilder = chain.request().newBuilder()
+
+        if (!tokenDataSource.authToken.isNullOrEmpty()) {
+            requestBuilder.addHeader("Authorization",
+                "Bearer ${tokenDataSource.authToken}")
+        }
+
+        requestBuilder.addHeader("X-MAL-Client-ID", client_id);
+
+        return chain.proceed(requestBuilder.build())
+    }
 
 }
