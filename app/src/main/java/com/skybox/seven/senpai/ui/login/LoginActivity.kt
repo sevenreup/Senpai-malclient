8bbbac7c9f1a532e596a05fc5bf039a6a5879ea7
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-07-02 16:09:18 +0200

    random work

---
+++ app/src/main/java/com/skybox/seven/senpai/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.skybox.seven.senpai.ui.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.skybox.seven.senpai.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/skybox/seven/senpai/ui/main/MainActivity.kt
+++ app/src/main/java/com/skybox/seven/senpai/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.skybox.seven.senpai
+package com.skybox.seven.senpai.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.skybox.seven.senpai.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
