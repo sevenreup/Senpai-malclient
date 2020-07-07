16a0cbf6d27b5347270c25c5fdb86bd30f3d896d
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-07-07 20:39:35 +0200

    checkpoint

--- app/src/main/java/com/skybox/seven/senpai/ui/anime/AnimeFragment.kt
+++ app/src/main/java/com/skybox/seven/senpai/ui/anime/AnimeFragment.kt
@@ -1,6 +1,7 @@
 package com.skybox.seven.senpai.ui.anime
 
 import android.os.Bundle
+import android.view.DragEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,7 +27,6 @@ class AnimeFragment : Fragment() {
         val adapter = AnimeViewPagerAdapter(childFragmentManager, lifecycle)
         binding.viewpager.adapter = adapter
         binding.pageIndicator.setViewPager2(binding.viewpager)
-
         return view
     }
 
