f235362b45115e28accbc7df33a8f725264e3fd2
Christopher Seven Phiri <chris7phiri@gmail.com>
2020-07-07 19:50:17 +0200

    viewpager and indicator setup

--- app/src/main/java/com/skybox/seven/senpai/ui/anime/AnimeFragment.kt
+++ app/src/main/java/com/skybox/seven/senpai/ui/anime/AnimeFragment.kt
@@ -6,16 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.skybox.seven.senpai.R
+import com.skybox.seven.senpai.adapter.AnimeViewPagerAdapter
+import com.skybox.seven.senpai.databinding.FragmentAnimeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_anime.*
 
 @AndroidEntryPoint
 class AnimeFragment : Fragment() {
+    private var _binding: FragmentAnimeBinding? = null
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val v = inflater.inflate(R.layout.fragment_anime, container, false)
-        return v
+        _binding = FragmentAnimeBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        val adapter = AnimeViewPagerAdapter(childFragmentManager, lifecycle)
+        binding.viewpager.adapter = adapter
+        binding.pageIndicator.setViewPager2(binding.viewpager)
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
